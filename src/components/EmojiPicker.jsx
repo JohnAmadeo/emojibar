24c4fb876b9d61235c5f6dc7d56a949f9f886eb4
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2017-07-05 23:34:15 -0700

    Improve method of getting cursor and emoji zone text (old method was buggy)

--- src/components/EmojiPicker.jsx
+++ src/components/EmojiPicker.jsx
@@ -10,59 +10,50 @@ export default class EmojiPicker extends Component {
     };
 
     this.getCursor = this.getCursor.bind(this);
-    this.inEmojiZone = this.inEmojiZone.bind(this);
+    this.getEmojiZoneText = this.getEmojiZoneText.bind(this);
     this.isWhitespaceCharacter = this.isWhitespaceCharacter.bind(this);
     this.trackTextEditor = this.trackTextEditor.bind(this);
     this.transformEmojiZonesToEmoji = this.transformEmojiZonesToEmoji.bind(this);
+    this.updateEmojiPicker = this.updateEmojiPicker.bind(this);
   }
 
   componentDidMount() {
     this.trackTextEditor();
   }
 
-/**
- * gets the position of the cursor following a change in the text editor's contents
- * @param {*} oldText - the inner text of the text editor before the latest change
- * @param {*} newText - the inner text of the text editor after the latest change
- */
-  getCursor(oldText, newText) {
-    const diffOperations = diff.diffChars(oldText, newText);
-
-    // if a block of text has been added, then the cursor is right after the last character in the inserted block of text
-    if (diffOperations.some(operation => operation.added)) {
-      return diffOperations
-        .slice(0, diffOperations.findIndex(operation => operation.added) + 1)
-        .reduce((index, operation) => (operation.removed ? index : index + operation.count), 0);
-    }
-    // if a block of text has been deleted, the cursor is right before the leftmost deleted character
-    else {
-      return diffOperations
-        .slice(0, diffOperations.findIndex(operation => operation.added || operation.removed))
-        .reduce((index, operation) => index + operation.count, 0);
-    }
-  }
-
   /**
-   * check if the cursor is in an emoji zone i.e if it is part of a continuous block of non-whitespace text beginning with a colon
-   * @param {string} text
-   * @param {integer} cursor - current index position of the cursor
+   * if the cursor is inside an emoji zone, return the text inside the emoji zone; else return an empty string
+   * @param {string} nodeText - text inside the data-block the cursor is currently in
+   * @param {integer} cursor - current index position of the cursor within the data-block it is in
    */
-  inEmojiZone(text, cursor) {
-    let seenColon = false;
-    for (let index = cursor - 1; index >= 0; index -= 1) {
-      if (seenColon) {
-        // all whitespace
-        return this.isWhitespaceCharacter(text[index]);
+  getEmojiZoneText(nodeText, cursor) {
+    let rightBound = 0;
+    let foundRightBound = false;
+    for (let index = cursor; index < nodeText.length; index += 1) {
+      if (this.isWhitespaceCharacter(nodeText[index])) {
+        break;
       }
-      // might have to be all whitespace chars
-      else if (this.isWhitespaceCharacter(text[index])) {
-        return false;
-      }
-      else if (text[index] === ':') {
-        seenColon = true;
+      else if (nodeText[index] === ':') {
+        rightBound = index;
+        foundRightBound = true;
       }
     }
-    return seenColon;
+
+    let leftBound = 0;
+    let foundLeftBound = false;
+    for (let index = cursor - 1; index >= 0; index -= 1) {
+      if (this.isWhitespaceCharacter(nodeText[index])) {
+        break;
+      }
+      else if (nodeText[index] === ':') {
+        if (index === 0 || this.isWhitespaceCharacter(nodeText[index - 1])) {
+          leftBound = index + 1;
+          foundLeftBound = true;
+        }
+      }
+    }
+
+    return foundLeftBound && foundRightBound ? nodeText.substring(leftBound, rightBound) : '';
   }
 
   /**
@@ -74,23 +65,41 @@ export default class EmojiPicker extends Component {
   }
 
   // Messenger textbox is a draft.js implementation - which sucks since input event doesn't fire in two weird cases, a) first character in the div, b) backspace/delete :(
+
+  /**
+   * Text in Messenger's content-editable wrapper div is split up into inner divs, where text between two adjacent divs are separated by a newline e.g the figure below
+   * <div data-contents=true>
+   *    <div data-block=true>
+   *      <div data-text=true>
+   *        text
+   *      </div>
+   *    </div>
+   *    <div data-block=true>
+   *      <div data-text=true>
+   *        text
+   *      </div>
+   *    </div>
+   *  </div>
+   */
+
   trackTextEditor() {
     const textEditor = document.querySelector('div[contenteditable=true]');
     textEditor.addEventListener('keydown', (event) => {
       const newInnerText = event.target.innerText;
       if (newInnerText !== this.state.innerText) {
-        // use diff library; needs to handle both insertion, deletion
-        const cursor = this.getCursor(newInnerText, this.state.innerText);
-        // a cursor is in the emoji zone if it is part of a block of string where '_:.*(cursor)'
-        const isInEmojiZone = this.inEmojiZone(newInnerText, cursor);
+        const cursor = document.getSelection().focusOffset;
+        const nodeText = document.getSelection().focusNode.wholeText;
+        const emojiZoneText = this.getEmojiZoneText(nodeText, cursor);
 
-        if (isInEmojiZone) {
+        // conditional implies cursor is in an emoji zone (i.e cursor is inside a continuous block of non-whitespace text started by a colon either at the start of a node or preceded by a whitespace character)
+        if (emojiZoneText !== '') {
           if (!this.state.isActive) {
             this.setState({
               isActive: true,
             });
           }
           // change the emoji picker focus to the emoji whose code most closely corresponds to the text of the current emoji zone
+          this.updateEmojiPicker(emojiZoneText);
         }
         // if cursor is not in an emoji zone and emoji picker was previously active, make emoji picker inactive
         else if (this.state.isActive) {
@@ -103,14 +112,25 @@ export default class EmojiPicker extends Component {
           innerText: newInnerText,
         });
 
-        // might have to pass in inner html since inner text can be spread across multiple span/divs due to paragraphing etc.
         this.transformEmojiZonesToEmoji(newInnerText);
       }
     });
   }
 
+  /**
+   * transform all closed emoji zones (i.e continuous blocks of text started by a colon preceded by a whitespace character and ended by a colon) into their corresponding emoji
+   * @param {string} text
+   */
   transformEmojiZonesToEmoji(text) {
 
+  }
+
+  /**
+   * updates the emojis being shown in the emoji picker depending on which emojis have codes closest to the string in the emoji zone
+   * @param {string} emojiZoneText - text in the emoji zone; may or may not match the code for an emoji
+   */
+  updateEmojiPicker(emojiZoneText) {
+    return emojiZoneText;
   }
 
   render() {
