59521e91ba7219e8006eb8d1558d31686f2cfd5f
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2017-07-30 23:49:12 -0700

    Filter set of supported emoji on Messenger

---
+++ src/scripts/get-supported-emoji.py
@@ -0,0 +1,33 @@
+import requests
+import pickle
+import json
+from bs4 import BeautifulSoup
+
+messenger_emojis = []
+emojilib = []
+supported_emoji_names = []
+
+r = requests.get('https://emojipedia.org/messenger/')
+main_page = BeautifulSoup(r.text, 'lxml')
+
+emoji_list_nodes = main_page.find('ul', class_='emoji-grid').find_all('li')
+
+for index, node in enumerate(emoji_list_nodes):
+    r = requests.get('https://emojipedia.org/' + node.a.attrs['href'])
+    emoji_page = BeautifulSoup(r.text, 'lxml')
+    messenger_emojis.append(emoji_page.find('input', id='emoji-copy').attrs['value'])
+    print(index)
+
+with open('src/data/messenger_emojis', 'w+') as file:
+    file.write(str(messenger_emojis))
+
+with open('node_modules/emojilib/emojis.json') as file:
+    emoji_dictionary = json.load(file)
+    emojilib = [{ **emoji_dictionary[key], **{ 'name': key }} for key in emoji_dictionary]
+
+for item in emojilib:
+    if item['char'] and item['char'] in messenger_emojis:
+        supported_emoji_names.append(item['name'])
+
+with open('src/data/supported_emojis', 'w+') as file:
+    file.write(str(supported_emoji_names))
