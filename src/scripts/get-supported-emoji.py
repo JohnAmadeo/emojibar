8189afb4ec5c302e391edbaa91a3fde0f6566950
Jeje2201 <jeremy_leriche@natural-solutions.eu>
2020-09-28 19:35:02 +0200

    new url list and encoding utf8

--- src/scripts/get-supported-emoji.py
+++ src/scripts/get-supported-emoji.py
@@ -7,7 +7,7 @@ messenger_emojis = []
 emojilib = []
 supported_emoji_names = []
 
-r = requests.get('https://emojipedia.org/messenger/')
+r = requests.get('https://emojipedia.org/facebook/')
 main_page = BeautifulSoup(r.text, 'lxml')
 
 emoji_list_nodes = main_page.find('ul', class_='emoji-grid').find_all('li')
@@ -18,10 +18,10 @@ for index, node in enumerate(emoji_list_nodes):
     messenger_emojis.append(emoji_page.find('input', id='emoji-copy').attrs['value'])
     print(index)
 
-with open('src/data/messenger_emojis', 'w+') as file:
+with open('src/data/messenger_emojis', 'w+', encoding="utf-8") as file:
     file.write(str(messenger_emojis))
 
-with open('node_modules/emojilib/emojis.json') as file:
+with open('node_modules/emojilib/emojis.json', encoding="utf-8") as file:
     emoji_dictionary = json.load(file)
     emojilib = [{ **emoji_dictionary[key], **{ 'name': key }} for key in emoji_dictionary]
 
@@ -29,5 +29,5 @@ for item in emojilib:
     if item['char'] and item['char'] in messenger_emojis:
         supported_emoji_names.append(item['name'])
 
-with open('src/data/supported_emojis', 'w+') as file:
+with open('src/data/supported_emojis', 'w+', encoding="utf-8") as file:
     file.write(str(supported_emoji_names))
